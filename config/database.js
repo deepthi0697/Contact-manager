f8f272bdd8b901e5a134600cca36ed5ad444d4a9
K N Deepthi <47886596+deepthi0697@users.noreply.github.com>
2020-04-09 00:34:26 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const setupDB = () => {
-    mongoose.connect('mongodb+srv://deepthi:shaz7022@cluster0-7uxoi.mongodb.net/Phone-Manager?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    mongoose.connect('mongodb+srv://deepthi:********@cluster0-7uxoi.mongodb.net/Phone-Manager?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(() => {
         console.log('connected to db')
     })
